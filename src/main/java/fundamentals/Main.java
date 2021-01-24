171cf47b46432053561351420bcd09430e88631c
corneliacalin <corneliacalin@yahoo.com>
2021-01-24 13:10:39 +0200

    Task 4 Sum of digits of a number

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        TasksExercises tasksExercises1= new TasksExercises();
-        TasksExercises tasksExercises2= new TasksExercises();
-        System.out.println("x="+tasksExercises1.x);
-        System.out.println("y="+tasksExercises1.y);
-        System.out.println("x2="+tasksExercises2.x);
-        System.out.println("y2="+tasksExercises2.y);
-        tasksExercises1.x=22;
-        TasksExercises.y=3;
-        System.out.println("x1="+tasksExercises1.x);
-        System.out.println("y1="+tasksExercises1.y);
-        System.out.println("x2="+tasksExercises2.x);
-        System.out.println("y2="+tasksExercises2.y);
+//        System.out.println("Hello world!");
+//        TasksExercises tasksExercises1= new TasksExercises();
+//        TasksExercises tasksExercises2= new TasksExercises();
+//        System.out.println("x="+tasksExercises1.x);
+//        System.out.println("y="+tasksExercises1.y);
+//        System.out.println("x2="+tasksExercises2.x);
+//        System.out.println("y2="+tasksExercises2.y);
+//        tasksExercises1.x=22;
+//        TasksExercises.y=3;
+//        System.out.println("x1="+tasksExercises1.x);
+//        System.out.println("y1="+tasksExercises1.y);
+//        System.out.println("x2="+tasksExercises2.x);
+//        System.out.println("y2="+tasksExercises2.y);
 
 //// task1
 //           tasksExercises.circlePerimeter();
@@ -28,8 +28,10 @@ public class Main {
  //       task2
 //        TasksExercises.bodyMassIndex();
         //Task 3
-        TasksExercises.fizzBuzz();
-        TasksExercises.primeNumber();
-        TasksExercises.fibonacciNumber();
+//        TasksExercises.fizzBuzz();
+//        TasksExercises.primeNumber();
+//        TasksExercises.fibonacciNumber();
+        //task4
+    TasksExercises.sumDigitNumber();
     }
 }
--- src/main/java/fundamentals/TasksExercises.java
+++ src/main/java/fundamentals/TasksExercises.java
@@ -110,7 +110,26 @@ public class TasksExercises {
         System.out.println("The number from Fibonacci sequence with index " + number + " is " +fib[number]);
 
     }
+//    Write an application that gets one positive number (type int) from the user and
+//    calculates a sum of digits of the given number. Hint: to make some operations on every
+//    single digit of the number (digit by digit), you can calculate the remainder of dividing
+//    the number by 10 (to get the value of the last digit) and divide the number by 10 (to "move" to the next digit).
+    public static void sumDigitNumber(){
+        System.out.println("Give a positive integer number: ");
+        Scanner scanner =new Scanner(System.in);
+                int number = scanner.nextInt();
+        int aux=number;
+        int sum=0;
+        while (aux!=0) {
+            sum = sum + aux % 10;
+            aux = aux / 10;
+        }
+        System.out.println("Sum of digits of number " + number + " is " + sum);
+        }
 
-}
+    }
+
+
+
 
 
